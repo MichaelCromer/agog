189696b2d8d1379f7323ea8d25f9b2e03ee8454d
Michael Cromer <mccromer.g@gmail.com>
2024-04-04 21:59:02 +1100

    clockon/off functions

--- src/main.c
+++ src/main.c
@@ -69,6 +69,11 @@ void list_tasks();
 void add_task(char *task_name);
 void remove_task(char *task_name);
 
+// Clock commands
+int agogo_clockon(int argc, char *argv[]);
+int agogo_clockoff();
+int is_clocked_on();
+
 /* ============================================================
  *  MAIN
  * ============================================================
@@ -93,9 +98,18 @@ int main(int argc, char *argv[])
   if (strcmp(first_command, "project") == 0) {
     exit_code = agogo_project(argc, argv);
   }
-
-  if (strcmp(first_command, "task") == 0) {
+  else if (strcmp(first_command, "task") == 0) {
     exit_code = agogo_task(argc, argv);
+  }
+  else if (strcmp(first_command, "clockon") == 0) {
+    exit_code = agogo_clockon(argc, argv);
+  }
+  else if (strcmp(first_command, "clockoff") == 0) {
+    exit_code = agogo_clockoff();
+  }
+  else {
+    printf("Error: Unknown command %s\n", first_command);
+    exit_code = EXIT_FAILURE;
   }
 
   return exit_code;
@@ -232,7 +246,6 @@ int agogo_task(int argc, char *argv[])
     }
     add_task(argv[3]);
   }
-
   if ((strcmp(sub_command, "--remove") == 0) || (strcmp(sub_command, "-r") == 0)) {
     if (argc < 4) {
       printf("Error: Missing task name\n");
@@ -240,6 +253,7 @@ int agogo_task(int argc, char *argv[])
     }
     remove_task(argv[3]);
   }
+
 
   return EXIT_SUCCESS;
 }
@@ -257,4 +271,77 @@ void add_task(char *task_name)
 void remove_task(char *task_name) 
 {
   printf("Removing task %s\n", task_name);
+}
+
+/* ============================================================
+ *  CLOCK
+ * ============================================================
+ */
+
+int agogo_clockon(int argc, char *argv[]) 
+{
+  if (argc < 3) {
+    printf("Error: Missing project name\n");
+    return EXIT_FAILURE;
+  }
+
+  if (strcmp(argv[2], "-p") == 0) {
+    if (argc < 4) {
+      printf("Error: Missing project name\n");
+      return EXIT_FAILURE;
+    }
+
+  }
+  char *project_name = argv[2];
+
+  // first check if the project exists
+  char project_path[256];
+  snprintf(project_path, sizeof(project_path), "%s/%s", AGOGO_PROJECTS_DIR, project_name);
+
+  char command[512];
+  snprintf(command, sizeof(command), "test -d %s", project_path);
+  int status = system(command);
+  if (status != 0) {
+    printf("Error: Project %s does not exist.\n", project_name);
+    exit(EXIT_FAILURE);
+  }
+
+  // then check if we are already clocked on to a project
+  if (is_clocked_on() == 0) {
+    system("rm " AGOGO_DIR "/current");
+  }
+
+  printf("Clocking on to project %s\n", project_name);
+  snprintf(command, sizeof(command), "ln -s %s %s/current", project_path, AGOGO_DIR);
+
+  status = system(command);
+  if (status != 0) {
+    printf("Error: Could not clock on to project %s.\n", project_name);
+    exit(EXIT_FAILURE);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int agogo_clockoff() 
+{
+  if (is_clocked_on() != 0) 
+  {
+    printf("Error: Not currently clocked on to any project.\n");
+    return EXIT_FAILURE;
+  }
+
+  printf("Clocking off\n");
+  int status = system("rm " AGOGO_DIR "/current");
+  if (status != 0) {
+    printf("Error: Could not clock off.\n");
+    exit(EXIT_FAILURE);
+  }
+  return EXIT_SUCCESS;
+}
+
+int is_clocked_on() 
+{
+  int status = system("test -L " AGOGO_DIR "/current");
+  return status;
 }
